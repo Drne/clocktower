7c6856617c77b9fae358b45bd7b4c04d1ed9aac4
Andrew Colgin <acolgin@northeastern.edu>
2023-07-08 16:07:50 -0400

    fix number inputs

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -5,14 +5,12 @@ import {Button, TextField} from "@mui/material";
 export const Admin = ({targetTime, startTime, updateTime}: {targetTime: Date, startTime: Date, updateTime: (time: number, startTime?: number) => void}) => {
     const [minutes, setMinutes] = useState<string>("");
     const [seconds, setSeconds] = useState<string>("");
-    const [pauseTime, setPauseTime] = useState<number>();
 
     const handleNumberTextFieldChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, setFunc: any) => {
         const str = e.target.value;
 
         setFunc(str)
     }
-
 
     return (
         <>
@@ -22,42 +20,27 @@ export const Admin = ({targetTime, startTime, updateTime}: {targetTime: Date, st
                 <TextField label={"Add Seconds"} value={seconds} onChange={(e) => handleNumberTextFieldChange(e, setSeconds)} />
 
                 <Button onClick={() => {
-                    if (seconds && minutes) {
                         const currentTime = Date.now()
 
-                        const parsedMins = parseInt(minutes)
-                        const parseSecs = parseInt(seconds)
+                        const parsedMins = minutes ? parseInt(minutes) : 0
+                        const parseSecs = seconds ? parseInt(seconds) : 0
                         updateTime(parseSecs * 1000 + parsedMins * 60000 + currentTime);
-                    }
                 }}>
                     Set Time
                 </Button>
                 <Button onClick={() => {
-                    if (seconds && minutes) {
-                        const parsedMins = parseInt(minutes)
-                        const parseSecs = parseInt(seconds)
-                        updateTime(parseSecs * 1000 + parsedMins * 60000 + targetTime.getTime(), startTime.getTime());
-                    }
+                    const parsedMins = minutes ? parseInt(minutes) : 0
+                    const parseSecs = seconds ? parseInt(seconds) : 0
+                    updateTime(parseSecs * 1000 + parsedMins * 60000 + targetTime.getTime(), startTime.getTime());
                 }}>
                     Add Time
                 </Button>
                 <Button onClick={() => {
-                    if (seconds && minutes) {
-                        const parsedMins = parseInt(minutes)
-                        const parseSecs = parseInt(seconds)
-                        updateTime(targetTime.getTime() - (parseSecs * 1000 + parsedMins * 60000), startTime.getTime());
-                    }
+                    const parsedMins = minutes ? parseInt(minutes) : 0
+                    const parseSecs = seconds ? parseInt(seconds) : 0
+                    updateTime(targetTime.getTime() - (parseSecs * 1000 + parsedMins * 60000), startTime.getTime());
                 }}>
                     Remove Time
-                </Button>
-                <Button onClick={() => {
-                    if (pauseTime) {
-
-                    } else {
-                        setPauseTime(Date.now())
-                    }
-                }}>
-                    {pauseTime ? 'Unpause' : 'Pause'}
                 </Button>
             </div>
         </>
