7c80d1361afb39adbd04d20bbc79952e9885abf7
Andrew Colgin <acolgin@northeastern.edu>
2023-10-07 13:59:09 -0400

    add sound effects, fix clock hand

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -5,19 +5,31 @@ import 'react-circular-progressbar/dist/styles.css';
 import {buildStyles, CircularProgressbarWithChildren} from 'react-circular-progressbar';
 import clock from '../files/clock.png';
 import demonHead from '../files/demon-head.png';
-import minutes from '../files/minutes.png'
+import minutes from '../files/minutes.png';
+import bellTower from '../files/bell-tower.mp3';
+import clockTicking from '../files/clock-ticking.mp3'
+import useSound from "use-sound";
 
 export const Countdown = ({timeToTarget, startTime}: {timeToTarget: Date, startTime: Date}) => {
     const [percentage, setPercentage] = useState(100);
 
+    const [playBell] = useSound(bellTower)
+    const [playClock] = useSound(clockTicking)
+
     const callback = useCallback(({ completed }: {completed: boolean}) => {
         if (completed) {
             setPercentage(100);
+            playBell()
         } else {
             const percentage = (Date.now() - startTime.getTime()) / (timeToTarget.getTime() - startTime.getTime())
             setPercentage(percentage * 100)
+
+            const secondsLeft = timeToTarget.getSeconds() - new Date().getSeconds()
+            if (secondsLeft === 30) {
+                playClock()
+            }
         }
-    }, [startTime, timeToTarget])
+    }, [playBell, playClock, startTime, timeToTarget])
 
     return (
         <div style={{position: 'relative', maxHeight: '80vh', maxWidth: '80vh', margin: 'auto', padding: '20px'}}>
@@ -27,7 +39,18 @@ export const Countdown = ({timeToTarget, startTime}: {timeToTarget: Date, startT
                         strokeLinecap: 'butt',
                     })
                 }>
-                <img src={minutes} alt={'clock-hand'} style={{position: 'absolute', maxWidth: '100%', maxHeight: '100%', zIndex: 20, opacity: percentage > 99 ? '10%' : '70%', transformOrigin: 'bottom', transform: `rotate(${Math.min(Math.max(0, percentage) / 100 * 360, 360)}deg)`, transition: '.5s ease', top: '6%'}}/>
+                <img src={minutes} alt={'clock-hand'} style={
+                    {
+                        position: 'absolute',
+                        height: '40%',
+                        zIndex: 20,
+                        opacity: percentage > 99 ? '10%' : '70%',
+                        transformOrigin: 'center 96%',
+                        transform: `rotate(${Math.min(Math.max(0, percentage) / 100 * 360, 360)}deg)`,
+                        transition: '.5s ease',
+                        top: '10%'
+                    }
+                }/>
                 <img src={clock} alt="clock-face" style={{position: 'absolute', maxWidth: '100%', maxHeight: '100%', zIndex: 25}} />
                 <FlipClockCountdown to={timeToTarget} renderMap={[false, false, true, true]} onTick={callback} style={{zIndex: 30}}>
                     <img src={demonHead} alt="demon-head" style={{ maxHeight: '60%', zIndex: 30}}/>
